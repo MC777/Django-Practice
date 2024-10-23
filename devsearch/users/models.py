543779065f83582b9b408d8065e212d4fd20e556
Marek <ciesla.marek@gmail.com>
2024-10-23 12:37:54 +0200

    add signals post_save and post_delete

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+from django.db.models.signals import post_save, post_delete
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -20,7 +21,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
     
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
@@ -31,3 +32,14 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+def profileUpdated(sender, instance, created, **kwargs):
+    print('Profile Saved!')
+    print('Instance', instance)
+    print('CREATED', created)
+
+def deleteUser(sender, instance, **kwargs):
+    print('Deliting user ...')
+
+post_save.connect(profileUpdated, sender=Profile)
+post_delete.connect(deleteUser, sender=Profile)
