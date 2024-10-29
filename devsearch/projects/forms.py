d87018eb80b933b887fb034cc2110b6c91dfea33
Marek <ciesla.marek@gmail.com>
2024-10-29 21:11:30 +0100

    update Review forms

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -2,7 +2,7 @@ from django.db.models.base import Model
 from django.forms import ModelForm, widgets
 from django import forms
 from django.forms.utils import ErrorList
-from .models import Project
+from .models import Project, Review
 
 class ProjectForm(ModelForm):
     class Meta:
@@ -14,6 +14,23 @@ class ProjectForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ProjectForm, self).__init__(*args, **kwargs)
+
+        # self.fields['title'].widget.attrs.update({'class':'input', 'placeholder': 'Add title'})
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+
+        lables = {
+            'values': 'Place your vote',
+            'body': 'Add a comment with your vote'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
 
         # self.fields['title'].widget.attrs.update({'class':'input', 'placeholder': 'Add title'})
         for name, field in self.fields.items():
