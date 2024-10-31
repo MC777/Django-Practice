9d24bacde9fde2da266b63236222b1c144588e57
Marek <ciesla.marek@gmail.com>
2024-10-31 20:40:35 +0100

    add code for sending welcome email in signal.py

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -3,6 +3,9 @@ from django.db.models.signals import post_save, post_delete
 from django.contrib.auth.models import User
 from .models import Profile
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 # @receiver(post_save, sender=Profile)
 def createProfile(sender, instance, created, **kwargs):
     if created:
@@ -13,6 +16,19 @@ def createProfile(sender, instance, created, **kwargs):
             email = user.email,
             name = user.first_name,
         )
+
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you are here!'
+
+        #TODO: modify google account for signals
+        # https://myaccount.google.com/lesssecureapps
+        # send_mail(
+        #     subject,
+        #     message,
+        #     settings.EMAIL_HOST_USER,
+        #     [profile.email],
+        #     fail_silently=False,
+        # )
 
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
